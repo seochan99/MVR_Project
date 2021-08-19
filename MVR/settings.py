f0af50707be2ad02bbb35f95ee8eb14d51e0d6b2
seochan99 <gmlcks0513@naver.com>
2021-08-19 14:30:39 +0900

    login

--- MVR/settings.py
+++ MVR/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'main',
     'mypage',
     'review',
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
 ]
 
 MIDDLEWARE = [
@@ -135,6 +139,15 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
+
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = "/"
--- MVR/urls.py
+++ MVR/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 from main import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.showmain, name="showmain"),
+    path('accounts/',include('allauth.urls')),
+    path('mypage/',include('mypage.urls')),
 ]
